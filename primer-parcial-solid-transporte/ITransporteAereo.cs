e78c3e7d972e8e97b05d7ce1f0cb216c5d24c896
dscoronel <dscoronel10@gmail.com>
2023-04-25 20:04:13 -0300

    Terminado

---
+++ primer-parcial-solid-transporte/ITransporteAereo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primer_parcial_solid_transporte
+{
+    public interface ITransporteAereo
+    {
+        void Volar(int altitud);
+    }
+}
